1f94700322cda53fcadaeb02613b23c0db4c9525
FelipeRoveroni <rm95430@fiap.com.br>
2023-05-09 22:05:05 -0300

    Exercicio 3 Finalizado

--- Exercicio3.sql
+++ Exercicio3.sql
@@ -1,39 +1,74 @@
 CREATE OR REPLACE PROCEDURE SP_DML_ENDERECO_CLIENTE(
-    p_operacao VARCHAR2,
-    p_retorno OUT VARCHAR2
+    pr_operacao VARCHAR2,
+    valor1 number,
+    valor2 number,
+    valor3 number,
+    valor4 VARCHAR2,
+    valor5 VARCHAR2,
+    valor6 VARCHAR2,
+    valor7 number,
+    valor8 VARCHAR2,
+    valor9 number,
+    valor10 char,
+    valor11 date,
+    valor12 VARCHAR2
 ) AS
+
+    v_execption EXCEPTION;
+
 BEGIN
-    IF (p_operacao = 'INSERT') THEN
-        -- Código para realizar o INSERT na tabela ENDERECO_CLIENTE
-        INSERT INTO ENDERECO_CLIENTE (coluna1, coluna2, coluna3)
-        VALUES (valor1, valor2, valor3);
-        
-        p_retorno := 'Registro inserido com sucesso.';
-    ELSIF (p_operacao = 'UPDATE') THEN
-        -- Código para realizar o UPDATE na tabela ENDERECO_CLIENTE
+    CASE UPPER(pn_operacao)
+    WHEN 'I' THEN
+        INSERT INTO ENDERECO_CLIENTE (SEQ_ENDERECO_CLIENTE, 
+        COD_TIPO_ENDERECO, 
+        COD_CLIENTE, 
+        DES_ENDERECO, 
+        NUM_ENDERECO, 
+        DES_COMPLEMENTO, 
+        NUM_CEP, 
+        DES_BAIRRO, 
+        COD_CIDADE, 
+        STA_ATIVO, 
+        DAT_CADASTRO, 
+        DAT_CANCELAMENTO
+        )
+        VALUES (
+        valor1, 
+        valor2, 
+        valor3,
+        valor4,
+        valor5,
+        valor6,
+        valor7,
+        valor8,
+        valor9,
+        valor10,
+        valor11,
+        valor12 
+        );
+    WHEN 'U' THEN
         UPDATE ENDERECO_CLIENTE
-        SET coluna1 = valor1,
-            coluna2 = valor2,
-            coluna3 = valor3
-        WHERE id = valor_id;
-        
-        p_retorno := 'Registro atualizado com sucesso.';
-    ELSIF (p_operacao = 'DELETE') THEN
-        -- Código para realizar o DELETE na tabela ENDERECO_CLIENTE
-        DELETE FROM ENDERECO_CLIENTE
-        WHERE id = valor_id;
-        
-        p_retorno := 'Registro excluído com sucesso.';
-    ELSE
-        -- Se o valor de p_operacao não for válido, lança uma exceção com uma mensagem de erro
-        RAISE_APPLICATION_ERROR(-20001, 'Operação inválida. Favor informar INSERT, UPDATE ou DELETE.');
-    END IF;
+        SET SEQ_ENDERECO_CLIENTE = valor1,
+            COD_TIPO_ENDERECO = valor2,
+            DES_ENDERECO = valor4,
+            NUM_ENDERECO = valor5,
+            DES_COMPLEMENTO = valor6,
+            NUM_CEP = valor7,
+            DES_BAIRRO = valor8,
+            COD_CIDADE = valor9,
+            STA_ATIVO = valor10,
+            DAT_CADASTRO = valor11,
+            DAT_CANCELAMENTO = valor12
+        WHERE COD_CLIENTE = valor3;
+    WHEN 'D' THEN
+        DELETE FROM ENDERECO_CLIENTE WHERE COD_CLIENTE = valor3;
+
+    END CASE;
     
-    -- Se a operação for realizada com sucesso, a mensagem de retorno é definida como 'Sucesso'
-    EXCEPTION
-        WHEN OTHERS THEN
-            p_retorno := 'Erro ao executar a operação. ' || SQLERRM;
+EXCEPTION
+  WHEN OTHERS
+  THEN
+    DBMS_OUTPUT.PUT_LINE ('ERRO ORACLE: ' || SQLCODE || SQLERRM);       
 END;
 
-DECLARE
-    v_retorno
+exec SP_DML_ENDERECO_CLIENTE()
